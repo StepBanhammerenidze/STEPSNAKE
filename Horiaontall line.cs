6497aa082b651711201c0b424b79471c4ca7729a
opilane <opilane@HP-CZC6297B9B>
2021-06-11 11:54:28 +0300

    Urok 7

---
+++ Horiaontall line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STEPSNAKE
+{
+    class Horiaontall_line
+    {
+        List<Point> pList;
+        public Horiaontall_line(int xLeft, int xRight, int y, char sym )
+        {
+            pList = new List<Point>();
+            for(int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Drow()
+        {
+            foreach( Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
